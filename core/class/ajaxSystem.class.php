6765cc9cbb8c037fb1092da88900969eb1b61332
Loïc <loic@jeedom.com>
2021-08-19 15:52:39 +0200

    same

--- core/class/ajaxSystem.class.php
+++ core/class/ajaxSystem.class.php
@@ -65,10 +65,10 @@ class ajaxSystem extends eqLogic {
     return $return;
   }
 
-  public static function cronHourly() {
+  public static function refreshAllData() {
     foreach (eqLogic::byType('ajaxSystem', true) as $eqLogic) {
       try {
-        sleep(rand(0, 600));
+        sleep(rand(0, 120));
         $eqLogic->refreshData();
       } catch (\Exception $e) {
         log::add('ajaxSystem', 'error', __('Erreur lors de la mise à jour des données de :', __FILE__) . ' ' . $eqLogic->getHumanName() . ' => ' . $e->getMessage(), 'ajaxSystem::failedGetData' . $eqLogic->getId());
