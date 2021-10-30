21ca880c52021c5aa945ae5387ef9ef5c523ad2d
loic <loic@jeedom.com>
2021-10-30 15:57:15 +0200

    continue

--- resources/ajaxSystemd/ajaxSystemd.py
+++ resources/ajaxSystemd/ajaxSystemd.py
@@ -22,6 +22,7 @@ import os
 import time
 import datetime
 import re
+from typing import Any
 import signal
 from optparse import OptionParser
 from os.path import join
@@ -36,12 +37,11 @@ except ImportError:
     sys.exit(1)
 
 
-events = []
-
-
 def func(event: SIAEvent):
-    logging.debug('Got an event : '+str(SIAEvent))
-    events.append(event)
+    info = get_event_data_from_sia_event(event)
+    if(info['code'] == 'RP'):
+        return
+    logging.debug('Got an event : ' + str(json.dumps(info)))
 
 
 def listen():
@@ -49,6 +49,50 @@ def listen():
     sia_client = SIAClient('', _siaport, account, function=func,
                            protocol=CommunicationsProtocol('TCP'))
     sia_client.start()
+
+
+def get_event_data_from_sia_event(event: SIAEvent) -> dict[str, Any]:
+    return {
+        "message_type": event.message_type.value,
+        "receiver": event.receiver,
+        "line": event.line,
+        "account": event.account,
+        "sequence": event.sequence,
+        "content": event.content,
+        "ti": event.ti,
+        "id": event.id,
+        "ri": event.ri,
+        "code": event.code,
+        "message": event.message,
+        "x_data": event.x_data,
+        "timestamp": event.timestamp.isoformat()
+        if event.timestamp
+        else utcnow().isoformat(),
+        "event_qualifier": event.event_qualifier,
+        "event_type": event.event_type,
+        "partition": event.partition,
+        "extended_data": [
+            {
+                "identifier": xd.identifier,
+                "name": xd.name,
+                "description": xd.description,
+                "length": xd.length,
+                "characters": xd.characters,
+                "value": xd.value,
+            }
+            for xd in event.extended_data
+        ]
+        if event.extended_data is not None
+        else None,
+        "sia_code": {
+            "code": event.sia_code.code,
+            "type": event.sia_code.type,
+            "description": event.sia_code.description,
+            "concerns": event.sia_code.concerns,
+        }
+        if event.sia_code is not None
+        else None,
+    }
 # ----------------------------------------------------------------------------
 
 
