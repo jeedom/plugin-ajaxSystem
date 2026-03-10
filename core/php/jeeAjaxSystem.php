4defbe1e4a91d29278466ba9044b1fee1239f75e
Loïc <1536036+zoic21@users.noreply.github.com>
2026-03-10 09:17:39 +0100

    Update jeeAjaxSystem.php

--- core/php/jeeAjaxSystem.php
+++ core/php/jeeAjaxSystem.php
@@ -70,7 +70,7 @@ foreach ($datas['data'] as $data) {
       if ($convert_key == 'realState') {
         $value = ($value == 0) ? 1 : 0;
       }
-      if($ajaxSystem->getConfiguration('device') == 'LightSwitchTwoWay' && $key == 'channelStatus'){
+      if($ajaxSystem->getConfiguration('device') == 'LightSwitchTwoChannelTwoWay' && $key == 'channelStatus'){
           switch ($value) {
               case 0:
                   $ajaxSystem->checkAndUpdateCmd('channelStatus_1', 0);
